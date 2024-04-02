4654c35b47c663152b2525c7c550737308c9640c
ym1085 <youngmin1085@gmail.com>
2024-04-03 01:38:15 +0900

    refactor: 장바구니 서비스에서 responseCode를 그냥 넘기는게 아니라 ResponseEntity를 넘기도록 리팩터링
    1. responseCode 0, 1 보다는 service 레이어에서 ResponseEntity 객체를 넘기도록 수정

--- app/shop/src/main/java/com/shoppingmall/service/CartService.java
+++ app/shop/src/main/java/com/shoppingmall/service/CartService.java
@@ -1,9 +1,15 @@
 package com.shoppingmall.service;
 
+import com.shoppingmall.common.response.ApiUtils;
+import com.shoppingmall.common.response.CommonResponse;
 import com.shoppingmall.common.response.ErrorCode;
+import com.shoppingmall.common.response.SuccessCode;
+import com.shoppingmall.dto.request.CartDeleteRequestDto;
+import com.shoppingmall.dto.request.CartDetailRequestDto;
 import com.shoppingmall.dto.request.CartSaveRequestDto;
 import com.shoppingmall.dto.request.CartUpdateRequestDto;
 import com.shoppingmall.dto.response.CartDetailResponseDto;
+import com.shoppingmall.dto.response.CartUUIDResponseDto;
 import com.shoppingmall.exception.FailDeleteCartProductException;
 import com.shoppingmall.exception.FailSaveCartProductException;
 import com.shoppingmall.exception.FailUpdateCartProductException;
@@ -12,10 +18,11 @@ import com.shoppingmall.mapper.MemberMapper;
 import com.shoppingmall.mapper.ProductMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -30,51 +37,58 @@ public class CartService {
     private final CartMapper cartMapper;
     private final ProductMapper productMapper;
 
+    /**
+     * 장바구니 등록 및 업데이트
+     *
+     * 'N' > 0 : 기존 장바구니 내역 존재하기에 장바구니 업데이트
+     * 'N' == 0 : 기존 장바구니 내역 존재하지 않기에 장바구니 등록
+     * 'N' < 0 : 예외 처리
+     */
     @Transactional
-    public void addCartProductForMember(CartSaveRequestDto cartRequestDto) {
-        if (!isExistsProduct(cartRequestDto)) {
+    public ResponseEntity<CommonResponse> addCartProductForMember(CartSaveRequestDto cartSaveRequestDto) {
+        if (!isExistsProduct(cartSaveRequestDto)) {
             throw new IllegalArgumentException("해당 상품이 존재하지 않습니다.");
         }
 
         // 해당 회원 장바구니에 등록된 상품이 있는지 확인
-        int cartProductsCount = cartMapper.countCartProducts(cartRequestDto.toEntity());
+        int cartProductsCount = cartMapper.countCartProducts(cartSaveRequestDto.toEntity());
+
+        ResponseEntity<CommonResponse> response;
         if (cartProductsCount > 0) {
-            if (cartMapper.updateCartProduct(cartRequestDto.toEntity()) < 1) {
-                throw new FailUpdateCartProductException(ErrorCode.UPDATE_CART);
-            }
+            response = updateCartProduct(cartSaveRequestDto);
+        } else if (cartProductsCount == 0) {
+            response = addCartProduct(cartSaveRequestDto);
         } else {
-            if (cartMapper.addCartProduct(cartRequestDto.toEntity()) < 1) {
-                throw new FailSaveCartProductException(ErrorCode.SAVE_CART);
-            }
+            // 0 미만은 오류로 판단 후 예외 발생!! -> 0 미만의 값이 DB의 장바구니 테이블에 들어가 있으면 안됨
+            throw new RuntimeException("장바구니 등록 및 업데이트 중 오류가 발생했습니다.");
         }
+        return response;
     }
 
-    @Transactional
-    public void updateCartProduct(CartUpdateRequestDto cartUpdateRequestDto) {
-        int responseCode = cartMapper.updateCartProduct(cartUpdateRequestDto.toEntity());
-        if (responseCode == 0) {
-            throw new FailUpdateCartProductException(ErrorCode.UPDATE_CART);
-        }
-    }
-
-    public List<CartDetailResponseDto> getCartItems(Integer memberId) {
-        if (memberId == null) {
-            return Collections.emptyList();
-        }
-
-        return cartMapper.getCartItems(memberId)
+    public List<CartDetailResponseDto> getCartItems(CartDetailRequestDto cartDetailRequestDto) {
+        List<CartDetailResponseDto> cartDetailResponseDtos = cartMapper.getCartItems(cartDetailRequestDto.toEntity())
                 .stream()
                 .filter(Objects::nonNull)
                 .map(CartDetailResponseDto::toDto)
                 .collect(Collectors.toList());
+
+        return cartDetailResponseDtos;
     }
 
     @Transactional
-    public void deleteCartItem(Integer cartId, Integer memberId) {
-        int responseCode = cartMapper.deleteCartItem(cartId, memberId);
-        if (responseCode == 0) {
+    public ResponseEntity<CommonResponse> updateCartProduct(CartUpdateRequestDto cartUpdateRequestDto) {
+        if (cartMapper.updateCartProduct(cartUpdateRequestDto.toEntity()) < 1) {
+            throw new FailUpdateCartProductException(ErrorCode.UPDATE_CART);
+        }
+        return buildSuccessResponse(SuccessCode.UPDATE_CART, cartUpdateRequestDto.getUuid());
+    }
+
+    @Transactional
+    public ResponseEntity<CommonResponse> deleteCartItem(CartDeleteRequestDto cartDeleteRequestDto) {
+        if (cartMapper.deleteCartItem(cartDeleteRequestDto.toEntity()) < 1) {
             throw new FailDeleteCartProductException(ErrorCode.DELETE_CART);
         }
+        return buildSuccessResponse(SuccessCode.DELETE_CART, cartDeleteRequestDto.getUuid());
     }
 
     private int getTotalPriceCartItems(Integer memberId) {
@@ -83,5 +97,34 @@ public class CartService {
 
     private boolean isExistsProduct(CartSaveRequestDto cartRequestDto) {
         return productMapper.getProductByProductId(cartRequestDto.getProductId()).isPresent(); // 상품이 먼저 존재하는 지 한번 더 검증
+    }
+
+    private ResponseEntity<CommonResponse> updateCartProduct(CartSaveRequestDto cartSaveRequestDto) {
+        if (cartMapper.updateCartProduct(cartSaveRequestDto.toEntity()) < 1) {
+            throw new FailUpdateCartProductException(ErrorCode.UPDATE_CART);
+        }
+        log.info("[addCartProductForMember] UPDATE cart product success");
+        return buildSuccessResponse(SuccessCode.UPDATE_CART, cartSaveRequestDto.getUuid());
+    }
+
+    private ResponseEntity<CommonResponse> addCartProduct(CartSaveRequestDto cartSaveRequestDto) {
+        if (cartMapper.addCartProduct(cartSaveRequestDto.toEntity()) < 1) {
+            throw new FailSaveCartProductException(ErrorCode.SAVE_CART);
+        }
+        log.info("[addCartProductForMember] INSERT cart product success");
+        return buildSuccessResponse(SuccessCode.SAVE_CART, cartSaveRequestDto.getUuid());
+    }
+
+    private ResponseEntity<CommonResponse> buildSuccessResponse(SuccessCode successCode, String uuid) {
+        return ApiUtils.success(
+                successCode.getCode(),
+                successCode.getHttpStatus(),
+                successCode.getMessage(),
+                StringUtils.isNotBlank(uuid) ? getCartUUIDResponseDto(uuid) : ""
+        );
+    }
+
+    private CartUUIDResponseDto getCartUUIDResponseDto(String uuid) {
+        return CartUUIDResponseDto.builder().uuid(uuid).build();
     }
 }
